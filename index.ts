8e73fdcd0e94a0b0a31feed8f130d51d79f8a76e
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-07-10 20:36:55 +0400

    finished creating program

--- index.ts
+++ index.ts
@@ -1,15 +1,84 @@
 require("dotenv").config();
+import * as sgMail from '@sendgrid/mail'
 
-import { createClient } from "@supabase/supabase-js";
+sgMail.setApiKey(process.env.SENDGRID_API??"")
+
+
+import { RealtimePostgresInsertPayload, createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL ?? "",
   process.env.SUPABASE_KEY ?? ""
 );
 
-async function subscribeToChanges () {
+
+function getNumberSuffix(num: number)
+{
+    var array = ("" + num).split("").reverse(); // E.g. 123 = array("3","2","1")
     
+    if (array[1] != "1") { // Number is not in the teens
+        switch (array[0]) {
+            case "1": return "st";
+            case "2": return "nd";
+            case "3": return "rd";
+        }
+    }
+    
+    return "th";
 }
 
 
+/**
+ * 
+ * @param payload RealtimePostgresInsertPayload from a Supabase subcribe event
+ */
+async function handleNewUser(
+  payload: RealtimePostgresInsertPayload<{
+    [key: string]: any;
+  }>
+) {
+    console.log('Detected User')
+    const {
+        displayname,
+        rank,
+        email_address
+    } = payload.new
+    
+    const msg = {
+        to: email_address,
+        from: process.env.FROM_EMAIL??"",
+        dynamic_template_data: {
+            "user_display_name": displayname,
+            "rank": `${rank}${getNumberSuffix(rank)}`
+        },
+        template_id: process.env.SENDGRID_TEMPLATE_ID,
+    }
 
+   // @ts-ignore
+    sgMail.send(msg)
+    .then(()=>{
+        console.log("Email sent to ", displayname, "{", email_address, "}")
+    })
+
+
+}
+
+function subscribeToChanges() {
+  console.log("Subscribing...");
+  const channel = supabase
+    .channel("table-db-changes")
+    .on(
+      "postgres_changes",
+      {
+        event: "INSERT",
+        schema: "public",
+        table: "profiles",
+      },
+      (payload) => {
+        handleNewUser(payload)
+      }
+    )
+    .subscribe();
+}
+
+subscribeToChanges();
